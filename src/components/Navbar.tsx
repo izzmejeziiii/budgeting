88e018a1eb4015a03b42a2cb438e07523b8a15c3
Jezi Anne Tobio <jezi.tobio@goabroad.com>
2024-05-12 22:18:51 +0800

    add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <div className="w-full py-5 shadow-md flex items-center justify-center">
+      <div className="max-w-7xl w-full items-center px-14 flex justify-between">
+        <a href="#">logo ?</a>
+        <div className="flex gap-2 items-center">
+          <div className="w-9 h-9 bg-slate-200 rounded-full"></div>
+          <p>ako ine ?</p>
+        </div>
+      </div>
+    </div>
+  );
+}
